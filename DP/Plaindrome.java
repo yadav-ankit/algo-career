e719fe52b1ddbbdb2d6db3282f2a2ba81dd7acbf
ANKIT YADAV <any65889@gmail.com>
2024-06-03 06:48:38 +0530

    Create Plaindrome.java

---
+++ DP/Plaindrome.java
@@ -0,0 +1,20 @@
+https://takeuforward.org/data-structure/longest-palindromic-subsequence-dp-28/
+
+/*
+The longest palindromic subsequence of a string is the longest common subsequence of the given string and its reverse.
+*/
+
+
+
+--------------------------------------
+
+
+https://takeuforward.org/data-structure/minimum-insertions-to-make-string-palindrome-dp-29/
+
+/*
+
+In order to minimize the insertions, we need to find the length of the longest palindromic component or in other words, the longest palindromic subsequence.
+
+Minimum Insertion required = n(length of the string) - length of longest palindromic subsequence. (see above)
+
+*/

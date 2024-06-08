dccd924e7de812dd950cfa4523e371ba4350dde7
ANKIT YADAV <any65889@gmail.com>
2024-06-08 07:12:49 +0530

    Update CombinationSumBoth.java

--- Recursion/CombinationSumBoth.java
+++ Recursion/CombinationSumBoth.java
@@ -25,7 +25,20 @@
 Problem Statement: Given a collection of candidate numbers (candidates) and a target number (target),
   find all unique combinations in candidates where the candidate numbers sum to target.
   Each number in candidates may only be used once in the combination.
-  
+
+ Input: candidates = [10,1,2,7,6,1,5], target = 8
+
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]]
+
+ make sure to sort the elements because the ans should contain the combinations in sorted order and should not be repeated.
+ Check if the current index value can be added to our ds. If yes add it to the ds and move the index by 1. 
+ while moving the index skip the consecutive repeated elements because they will form duplicate sequences.
+ 
  static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
         if (target == 0) {
             ans.add(new ArrayList < > (ds));

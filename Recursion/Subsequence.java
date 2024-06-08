97ff664a9e2e256fb15516191491401eac050f82
ANKIT YADAV <any65889@gmail.com>
2024-06-08 07:01:50 +0530

    Update Subsequence.java

--- Recursion/Subsequence.java
+++ Recursion/Subsequence.java
@@ -13,7 +13,6 @@ public class SubsetSum {
                 for (int num : ds) {
                     System.out.print(num + " ");
                 }
-                System.out.println();
             }
             return;
         }
@@ -39,6 +38,49 @@ public class SubsetSum {
         printS(0, ds, 0, sum, arr, n);
     }
 }
+
+
+-----------------------
+// print any one  use T / F    
+
+public class SubsetSum {
+
+    // Function to find and print all subsets that sum up to a given value
+    public static boolean printS(int ind, List<Integer> ds, int s, int sum, int[] arr, int n) {
+        if (ind == n) {
+            if (s == sum) {
+                for (int num : ds) {
+                    System.out.print(num + " ");
+                }
+                return true; // Condition satisfied
+            }
+            return false; // Condition not satisfied
+        }
+
+        // Pick the current element
+        ds.add(arr[ind]);
+        s += arr[ind];
+        if (printS(ind + 1, ds, s, sum, arr, n)) {
+            return true;
+        }
+
+        // Backtrack by removing the current element
+        s -= arr[ind];
+        ds.remove(ds.size() - 1);
+
+        // Do not pick the current element
+        if (printS(ind + 1, ds, s, sum, arr, n)) {
+            return true;
+        }
+
+        return false; // No valid subset found
+    }
+}
+
+--------------------
+
+
+// count all subseqnece whose sum == k
 
 
 

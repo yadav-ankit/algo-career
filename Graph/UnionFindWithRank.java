b18384bad984ffacfdf5ea60979e9ccaa1a651ff
ANKIT YADAV <any65889@gmail.com>
2025-12-21 06:16:29 +0530

    Create UnionFindWithRank.java

---
+++ Graph/UnionFindWithRank.java
@@ -0,0 +1,33 @@
+class UnionFind {
+    int[] parent;
+    int[] rank;
+
+    UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]); // path compression
+        return parent[x];
+    }
+
+    void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb) return;
+
+        if (rank[pa] < rank[pb]) {
+            parent[pa] = pb;
+        } else if (rank[pb] < rank[pa]) {
+            parent[pb] = pa;
+        } else {
+            parent[pb] = pa;
+            rank[pa]++;
+        }
+    }
+}

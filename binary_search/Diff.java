259fb87123a7476d1fe8ec3a5cebaf6752c930eb
ANKIT YADAV <any65889@gmail.com>
2026-01-10 06:28:29 +0530

    Create Diff.java

---
+++ binary_search/Diff.java
@@ -0,0 +1,25 @@
+
+
+Rotated Sorted Array
+[4,5,6,7,0,1,2]
+        ↑ pivot
+
+
+Entire array was sorted ascending
+
+Then rotated
+
+At least one half is always sorted
+
+Mountain Array
+[1,3,5,7,6,4,2]
+        ↑ peak
+
+
+Left half ascending
+
+Right half descending
+
+No rotation
+
+Sorting direction changes

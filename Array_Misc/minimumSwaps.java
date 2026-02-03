8f8e874d2a7f9101e4597df51218f552722810c4
ANKIT YADAV <any65889@gmail.com>
2026-02-03 06:57:17 +0530

    Create minimumSwaps.java

---
+++ Array_Misc/minimumSwaps.java
@@ -0,0 +1,49 @@
+Description
+You are given a 0-indexed integer array nums.
+
+Swaps of adjacent elements are able to be performed on nums.
+
+A valid array meets the following conditions:
+
+The largest element (any of the largest elements if there are multiple) is at the rightmost position in the array.
+The smallest element (any of the smallest elements if there are multiple) is at the leftmost position in the array.
+Return the minimum swaps required to make nums a valid array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,5,3,1]
+Output: 6
+Explanation: Perform the following swaps:
+- Swap 1: Swap the 3rd and 4th elements, nums is then [3,4,5,3,5,1].
+- Swap 2: Swap the 4th and 5th elements, nums is then [3,4,5,3,1,5].
+- Swap 3: Swap the 3rd and 4th elements, nums is then [3,4,5,1,3,5].
+- Swap 4: Swap the 2nd and 3rd elements, nums is then [3,4,1,5,3,5].
+- Swap 5: Swap the 1st and 2nd elements, nums is then [3,1,4,5,3,5].
+- Swap 6: Swap the 0th and 1st elements, nums is then [1,3,4,5,3,5].
+It can be shown that 6 swaps is the minimum swaps required to make a valid array.
+Example 2:
+
+Input: nums = [9]
+Output: 0
+Explanation: The array is already valid, so we return 0.
+
+class Solution {
+    public int minimumSwaps(int[] nums) {
+        int n = nums.length;
+        int i = 0, j = 0;
+        for (int k = 0; k < n; ++k) {
+            if (nums[k] < nums[i] || (nums[k] == nums[i] && k < i)) {
+                i = k;
+            }
+            if (nums[k] > nums[j] || (nums[k] == nums[j] && k > j)) {
+                j = k;
+            }
+        }
+        if (i == j) {
+            return 0;
+        }
+        return i + n - 1 - j - (i > j ? 1 : 0);
+    }
+}

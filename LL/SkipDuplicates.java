df51f61464238580a8324c531e464bb26a7e2f25
ANKIT YADAV <any65889@gmail.com>
2026-02-16 06:46:44 +0530

    Create SkipDuplicates.java

---
+++ LL/SkipDuplicates.java
@@ -0,0 +1,26 @@
+Given a sorted linked list, delete all duplicates such that each element appear
+only once.
+Example 1:
+Input: 1->1->2
+Output: 1->2
+Example 2:
+Input: 1->1->2->3->3
+Output: 1->2->3
+  
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next; // skip duplicate
+            } else {
+                curr = curr.next; // move forward
+            }
+        }
+
+        return head;
+    }
+}

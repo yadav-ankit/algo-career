0821465bac2fd47e36c3f7c5ac0db4acbd45359b
ANKIT YADAV <any65889@gmail.com>
2026-05-01 05:53:22 +0530

    Update Simple.java

--- tree/Simple.java
+++ tree/Simple.java
@@ -12,3 +12,29 @@ class Solution {
             && isMirror(l.right, r.left);
     }
 }
+
+
+https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/description/
+
+class FindElements {
+
+    private Set<Integer> set = new HashSet<>();
+
+    public FindElements(TreeNode root) {
+        recover(root, 0);
+    }
+
+    private void recover(TreeNode node, int val) {
+        if (node == null) return;
+
+        node.val = val;
+        set.add(val);
+
+        recover(node.left, 2 * val + 1);
+        recover(node.right, 2 * val + 2);
+    }
+
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+}

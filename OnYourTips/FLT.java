5920ccb046a421c29d698bb633576bca0630b7ff
ANKIT YADAV <any65889@gmail.com>
2026-05-02 11:45:41 +0530

    Update FLT.java

--- OnYourTips/FLT.java
+++ OnYourTips/FLT.java
@@ -3,3 +3,24 @@ Fermats little theorem..
 If we want to do Mod p on division  like we want to do (a / b) % p
 
 then we have to use this --> (a * b^(p-2)) % p
+
+  ----------------
+
+  
+  Now this power is also do be done like this --> modPow(b, p - 2, p);
+
+
+  static long modPow(long base, long exp, long mod) {
+        long result = 1;
+        base = base % mod;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+

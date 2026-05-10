b24c7c6b4c82996beb079dc5e14fa0f688b043d5
ANKIT YADAV <any65889@gmail.com>
2026-05-10 06:28:06 +0530

    Update arthimetic_slices.java

--- DP/arthimetic_slices.java
+++ DP/arthimetic_slices.java
@@ -56,3 +56,50 @@ class Solution {
         return ans;
     }
 }
+
+
+--------------------
+https://leetcode.com/problems/arithmetic-slices/description/
+    
+An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+
+For example, [1,3,5,7,9], [7,7,7,7], and [3,-1,-5,-9] are arithmetic sequences.
+Given an integer array nums, return the number of arithmetic subarrays of nums.
+
+A subarray is a contiguous subsequence of the array.
+
+
+    
+    class Solution {
+
+    public int numberOfArithmeticSlices(int[] nums) {
+
+        int n = nums.length;
+
+        if (n < 3) {
+            return 0;
+        }
+
+        int curr = 0;
+        int ans = 0;
+
+        for (int i = 2; i < n; i++) {
+
+            // Check if current triplet forms arithmetic sequence
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+
+                // Extend previous arithmetic slices
+                curr++;
+
+                ans += curr;
+
+            } else {
+
+                // Break in arithmetic pattern
+                curr = 0;
+            }
+        }
+
+        return ans;
+    }
+}

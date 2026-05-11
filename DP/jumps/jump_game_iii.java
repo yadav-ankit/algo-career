5b2a2890327f17dac405781196d7fd7816ef1ad3
ANKIT YADAV <any65889@gmail.com>
2026-05-11 06:24:16 +0530

    Create jump_game_iii.java

---
+++ DP/jumps/jump_game_iii.java
@@ -0,0 +1,57 @@
+https://leetcode.com/problems/jump-game-iii/description/
+
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+⏱ Complexity
+
+Each index processed once:
+
+O(n) time
+
+O(n) space
+
+
+class Solution {
+
+    public boolean canReach(int[] arr, int start) {
+
+        int n = arr.length;
+
+        Queue<Integer> q = new LinkedList<>();
+
+        boolean[] visited = new boolean[n];
+
+        q.offer(start);
+
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+
+            int idx = q.poll();
+
+            if (arr[idx] == 0) return true;
+
+            int forward = idx + arr[idx];
+
+            int backward = idx - arr[idx];
+
+            if (forward < n && !visited[forward]) {
+
+                visited[forward] = true;
+
+                q.offer(forward);
+            }
+
+            if (backward >= 0 && !visited[backward]) {
+
+                visited[backward] = true;
+
+                q.offer(backward);
+            }
+        }
+
+        return false;
+    }
+}

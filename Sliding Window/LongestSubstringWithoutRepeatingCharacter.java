91ac9c1b27aacceca9cb26cf212e1699db680bbb
ANKIT YADAV <any65889@gmail.com>
2026-05-25 06:24:10 +0530

    Update LongestSubstringWithoutRepeatingCharacter.java

--- Sliding Window/LongestSubstringWithoutRepeatingCharacter.java
+++ Sliding Window/LongestSubstringWithoutRepeatingCharacter.java
@@ -32,29 +32,21 @@ if our map size is = window (j-i+1) ..--> keep updating max
 else if map size < (j-i+1) this means map me duplicates h so increment i and remove duplicates 
     */
     
-  class Solution { 
-    public int lengthOfLongestSubstring(String s) { 
-        Map<Character, Integer> map = new HashMap<>(); 
-        int i = 0; 
-        int j = 0; 
-        int max = 0; 
-      
-        while(j < s.length()){ 
-            map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
-          
-            if(map.size() == j - i + 1){ 
-                max = Math.max(max, j - i + 1); 
-                j++; 
-            } 
-            else if(map.size() < j - i + 1){ 
-                while(map.size() < j - i + 1){ 
-                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1); 
-                    if(map.get(s.charAt(i)) == 0) map.remove(s.charAt(i)); 
-                    i++; 
-                } 
-                j++; 
-            } 
-        } 
-        return max; 
-    } 
-} 
+ public int lengthOfLongestSubstring(String s) {
+    Map<Character, Integer> map = new HashMap<>();
+    int i = 0, max = 0;
+
+    for(int j = 0; j < s.length(); j++) {
+        map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+
+        while(map.get(s.charAt(j)) > 1) {
+            map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+            if(map.get(s.charAt(i)) == 0) map.remove(s.charAt(i));
+            i++;
+        }
+
+        max = Math.max(max, j - i + 1);
+    }
+
+    return max;
+}

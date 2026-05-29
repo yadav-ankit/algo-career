e53f2085ff0faf5a5de92b2a3af8f1909b3c3044
ANKIT YADAV <any65889@gmail.com>
2026-05-29 06:14:00 +0530

    Update UniquePermutations_II.java

--- Recursion/UniquePermutations_II.java
+++ Recursion/UniquePermutations_II.java
@@ -1,4 +1,45 @@
+https://leetcode.com/problems/permutations/description/
+
+class Solution {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrack(nums, used, new ArrayList<>(), ans);
+
+        return ans;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> curr, List<List<Integer>> ans) {
+
+        if(curr.size() == nums.length){
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(used[i]){
+                continue;
+            }
+
+            used[i] = true;
+            curr.add(nums[i]);
+
+            backtrack(nums, used, curr, ans);
+
+            curr.remove(curr.size() - 1);
+            used[i] = false;
+        }
+    }
+}
+
+
+
 https://leetcode.com/problems/permutations-ii/description/
+// duplicates in array
 
 class Solution {
 
